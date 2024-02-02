48daad502a80f409ccd24a4e342e5bd3940924ac
Alisson Diniz <71110962+alissondiniz20@users.noreply.github.com>
2024-02-02 18:35:15 -0300

    finish pagination

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function index() {
-        return 'produtos';
+        $findProduct = Product::all();
+        
+        return view('pages.products.pagination', compact('findProduct'));
     }
 }
